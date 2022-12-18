63b46b36ea596cb4c9395b2dcd7f75c4105ed396
Andrew-Shatokhin <110032255+Andrew-Shatokhin@users.noreply.github.com>
2022-12-18 14:00:44 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
 
+import throttle from 'lodash.throttle'
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+form.addEventListener("submit", onFormSubmit);
+form.addEventListener("input", throttle(onFormData, 500));
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormData() {
+  const objectForm = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(objectForm));
+}
+
+localStorageData();
+
+function localStorageData() {
+  let savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    email.value = savedData.email;
+    message.value = savedData.message;
+  }
+}
