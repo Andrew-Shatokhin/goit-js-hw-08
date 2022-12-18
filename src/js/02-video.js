9ceb59751fc7e3ea999c3118b4e85dae0260ec75
Andrew-Shatokhin <110032255+Andrew-Shatokhin@users.noreply.github.com>
2022-12-18 02:04:56 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+function timeUpdate(evt) {
+    
+    localStorage.setItem('videoplayer-current-time', evt.seconds);
+};
+
+const getTimeUpdate = (localStorage.getItem('videoplayer-current-time'));
+if (getTimeUpdate !== null) {
+    player.setCurrentTime(getTimeUpdate)
+
+}
